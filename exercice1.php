cced66a7f0dab3b916f844358be36707533c4db8
Achille kouassi <patriceachillekouassin@gmail.com>
2023-02-08 20:33:39 +0100

    first commit

---
+++ exercice1.php
@@ -0,0 +1,45 @@
+<?php
+
+// Démarrage de la session
+session_start();
+
+// Variables pour afficher un message d'erreur
+$msg = '';
+
+// Vérification de la soumission du formulaire
+if (isset($_POST['login']) && !empty($_POST['username']) && !empty($_POST['password'])) {
+  // Récupération des données du formulaire
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+
+  // Vérification des données avec vos informations d'identification
+  if ($username == 'admin' && $password == 'password') {
+    $_SESSION['valid'] = true;
+    $_SESSION['timeout'] = time();
+    $_SESSION['username'] = 'admin';
+
+    // Redirection vers la page protégée
+    header('Location: protected_page.php');
+  } else {
+    $msg = 'Nom d\'utilisateur ou mot de passe incorrect.';
+  }
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Connexion</title>
+  </head>
+  <body>
+    <form action="..." method="post">
+      <h4 class="form-signin-heading"><?php echo $msg; ?></h4>
+      <input type="text" name="username" placeholder="username" required />
+      <input type="password" name="password" placeholder="password" required />
+      <button type="submit" name="login">Login</button>
+    </form>
+  </body>
+</html>
+
